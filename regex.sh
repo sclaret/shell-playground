0d9e22e26a0a24c6460ca00edbccfedaffe4d75d
Simon Claret <sclaret@gmail.com>
2018-10-28 16:03:47 -0400

    Lift wrapper function

--- regex.sh
+++ regex.sh
@@ -1,38 +1,43 @@
 #! /usr/bin/env bash
 
-function test_regex {
+function process_lines {
   while read pattern line; do
-    echo "$line =~ $pattern: "
-    [[ $line =~ $pattern ]]
-    case $? in
-      0) echo "Match: ${BASH_REMATCH[@]}"
-         ;;
-      1) echo "NO MATCH";;
-      2) echo "Error: regular expression is syntactically incorrect."
-         ;;
-      *) echo "Unknown error."
-         ;;
-    esac
+    test_regex $pattern $line
     echo
   done
 }
 
+function test_regex {
+  echo "$1 =~ $2: "
+  [[ $1 =~ $2 ]]
+  case $? in
+    0) echo "Match: ${BASH_REMATCH[@]}"
+       ;;
+    1) echo "NO MATCH";;
+    2) echo "Error: regular expression is syntactically incorrect."
+       ;;
+    *) echo "Unknown error."
+       ;;
+  esac
+}
+
 pattern="^(/Volumes/[^/]+)(.*)$"
 p=$pattern
-# echo $p /Volumes/seagate_hdd/restic-utils | test_regex $pattern
-# exit
 
-test_regex <<EOF
-$p /home/sclaret
-$p /Volumes/seagate_hdd/restic-utils
-$p /Volumes/seagate_hdd/restic-utils/
-$p /Volumes/seagate_hdd/restic-utils/lib
-$p /Volumes/seagate_hdd/restic-utils/lib/
-$p /Volumes/seagate_hdd/
-$p /Volumes/seagate_hdd
-$p /Volumes/seag
-$p /Volumes/a
-$p /Volumes/
-$p /Volumes
-$p /volumes/seag
+#test_regex /Volumes/seagate_hdd/restic-utils $p
+#exit
+
+process_lines <<EOF
+/home/sclaret $p
+/Volumes/seagate_hdd/restic-utils $p
+/Volumes/seagate_hdd/restic-utils/ $p
+/Volumes/seagate_hdd/restic-utils/lib $p
+/Volumes/seagate_hdd/restic-utils/lib/ $p
+/Volumes/seagate_hdd/ $p
+/Volumes/seagate_hdd $p
+/Volumes/seag $p
+/Volumes/a $p
+/Volumes/ $p
+/Volumes $p
+/volumes/seag $p
 EOF
